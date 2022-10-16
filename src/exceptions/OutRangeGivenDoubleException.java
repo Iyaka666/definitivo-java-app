b4f670ddb30f542bdafa58083725d569a4d0a7c5
Iyaka666 <duendecum@gmail.com>
2022-10-15 19:21:14 -0500

    rename file

---
+++ src/exceptions/OutRangeGivenDoubleException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptions;
+
+/**
+ *
+ * @author Marlon E. Zambrano Z.
+ */
+public class OutRangeGivenDoubleException extends Exception {
+
+    public OutRangeGivenDoubleException(String el_valor_no_esta_dentro_del_rango) {
+        super();
+    }
+    
+}
