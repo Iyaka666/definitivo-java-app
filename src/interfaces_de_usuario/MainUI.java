a425e894639666215d1483ae7dc8b26cc813121f
Iyaka666 <duendecum@gmail.com>
2022-10-17 11:56:29 -0500

    fixing somethings

--- src/interfaces_de_usuario/MainUI.java
+++ src/interfaces_de_usuario/MainUI.java
@@ -12,37 +12,54 @@ public class MainUI extends javax.swing.JFrame {
     public MainUI( Almacen thisStore ) {
         this.store = thisStore;
         initComponents();
-        jMCustomer.addActionListener(new HandlerShowCustomersUI());
-        jMProduct.addActionListener(new HandlerShowProductsUI());
-        jMTypeProduct.addActionListener(new HandlerShowTypeProductsUI());
-        jMPurchase.addActionListener(new HandlerShowSignInPurchaseUI());
+        jMICustomer.addActionListener(new HandlerShowCustomersUI());
+        jMIProducts.addActionListener(new HandlerShowProductsUI());
+        jMITypeProducts.addActionListener(new HandlerShowTypeProductsUI());
+        jMIPurchases.addActionListener(new HandlerShowSignInPurchaseUI());
+        jMIExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setDefaultCloseOperation(EXIT_ON_CLOSE);
+            }
+        });
     }
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jDesktopP = new javax.swing.JDesktopPane();
         jMB1 = new javax.swing.JMenuBar();
-        jMPurchase = new javax.swing.JMenu();
-        jMCustomer = new javax.swing.JMenu();
-        jMProduct = new javax.swing.JMenu();
-        jMTypeProduct = new javax.swing.JMenu();
+        jMMain = new javax.swing.JMenu();
+        jMIPurchases = new javax.swing.JMenuItem();
+        jMIProducts = new javax.swing.JMenuItem();
+        jMITypeProducts = new javax.swing.JMenuItem();
+        jMICustomer = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        jMIExit = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jDesktopP.setBackground(new java.awt.Color(255, 255, 255));
         getContentPane().add(jDesktopP, java.awt.BorderLayout.CENTER);
 
-        jMPurchase.setText("Compras");
-        jMB1.add(jMPurchase);
+        jMMain.setText("Principal");
 
-        jMCustomer.setText("Clientes");
-        jMB1.add(jMCustomer);
+        jMIPurchases.setText("Compras");
+        jMMain.add(jMIPurchases);
 
-        jMProduct.setText("Productos");
-        jMB1.add(jMProduct);
+        jMIProducts.setText("Productos");
+        jMMain.add(jMIProducts);
 
-        jMTypeProduct.setText("Tipos de productos");
-        jMB1.add(jMTypeProduct);
+        jMITypeProducts.setText("Tipos de Productos");
+        jMMain.add(jMITypeProducts);
+
+        jMICustomer.setText("Clientes");
+        jMMain.add(jMICustomer);
+        jMMain.add(jSeparator1);
+
+        jMIExit.setText("Salir");
+        jMMain.add(jMIExit);
+
+        jMB1.add(jMMain);
 
         setJMenuBar(jMB1);
 
@@ -52,14 +69,18 @@ public class MainUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane jDesktopP;
     private javax.swing.JMenuBar jMB1;
-    private javax.swing.JMenu jMCustomer;
-    private javax.swing.JMenu jMProduct;
-    private javax.swing.JMenu jMPurchase;
-    private javax.swing.JMenu jMTypeProduct;
+    private javax.swing.JMenuItem jMICustomer;
+    private javax.swing.JMenuItem jMIExit;
+    private javax.swing.JMenuItem jMIProducts;
+    private javax.swing.JMenuItem jMIPurchases;
+    private javax.swing.JMenuItem jMITypeProducts;
+    private javax.swing.JMenu jMMain;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
     // End of variables declaration//GEN-END:variables
         
     public class HandlerShowCustomersUI implements ActionListener{
-        private GestionarClientes customerUI;
+        private GestionarClientes customerUI = null;
+        
         @Override
         public void actionPerformed(ActionEvent e) {
             if(customerUI == null){
@@ -72,7 +93,8 @@ public class MainUI extends javax.swing.JFrame {
     }
     
     public class HandlerShowProductsUI implements ActionListener{
-        private GestionarProductos productUI;
+        private GestionarProductos productUI = null;
+        
         @Override
         public void actionPerformed(ActionEvent e) {
             if(productUI == null){
@@ -85,7 +107,8 @@ public class MainUI extends javax.swing.JFrame {
     }
     
     public class HandlerShowTypeProductsUI implements ActionListener{
-        private GestionarTiposProductos typeProductsUI;
+        private GestionarTiposProductos typeProductsUI = null;
+        
         @Override
         public void actionPerformed(ActionEvent e) {
             if(typeProductsUI == null){
@@ -98,7 +121,8 @@ public class MainUI extends javax.swing.JFrame {
     }
     
     public class HandlerShowSignInPurchaseUI implements ActionListener{
-        private RegistrarCompra signInPurchasesUI;
+        private RegistrarCompra signInPurchasesUI = null;
+        
         @Override
         public void actionPerformed(ActionEvent e) {
             if(signInPurchasesUI == null){
