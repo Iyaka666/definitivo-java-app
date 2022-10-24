46199db468db11bf54deb863210a24fe90ab6887
Iyaka666 <duendecum@gmail.com>
2022-10-23 20:17:32 -0500

    i removed two imports

--- src/interfaces_de_usuario/RegistrarCompra.java
+++ src/interfaces_de_usuario/RegistrarCompra.java
@@ -13,8 +13,6 @@ import java.time.LocalDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
 import javax.swing.table.AbstractTableModel;
 
 /**
